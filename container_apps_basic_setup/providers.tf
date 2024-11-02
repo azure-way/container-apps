d3faf988422c1fb193042d9ad70492981c13b757
Karol - Azure Way <karol@pieciukiewicz.com>
2024-11-02 22:31:05 +0100

    Add missing variables

--- container_apps_basic_setup/providers.tf
+++ container_apps_basic_setup/providers.tf
@@ -13,10 +13,15 @@ terraform {
   }
 
   required_version = ">= 1.1.0"
+
+  backend "azurerm" { }  
 }
 
 provider "azurerm" {
   features {}
 
   subscription_id = var.subscription-id
+  client_id       = var.spn-client-id
+  client_secret   = var.spn-client-secret
+  tenant_id       = var.spn-tenant-id
 }
